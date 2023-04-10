d926478ee12610cfb1ca39e0f66858c65050f0f4
Luke Brevoort <ljbrevoort@gmail.com>
2023-04-10 13:31:03 -0400

    Created DestinationList and DestinationCard components, added banner and logo to Home view.

---
+++ vue/src/components/DestinationList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="list">
+        <destination-card />
+    </div>
+</template>
+
+<script>
+import destinationCard from "../components/DestinationCard.vue";
+
+export default {
+    components: {
+      destinationCard
+    },
+    data () {
+        return {
+            destinations: []
+        }
+    }
+  };
+</script>
+
+<style>
+</style>
