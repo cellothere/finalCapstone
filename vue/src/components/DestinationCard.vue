d78e5827002785d8d57e0e6840b56fe2c14f583d
Ammar Bhatti <ammarbhatti.ab@gmail.com>
2023-04-14 11:27:29 -0400

    Reorganized Home

--- vue/src/components/DestinationCard.vue
+++ vue/src/components/DestinationCard.vue
@@ -8,7 +8,7 @@
                 <img class="thumbnail" :src="destination.imageUrl" />
                 <h3 >{{ destination.name }}</h3>
                 <div class="button-container">
-                    <label for="favorite"> Add to list: </label>
+                    <label for="favorite"> Favorite: </label>
                     <input type="checkbox" id="favorite" name="favorite" value="yes" v-model="selected" @change.prevent="addToFavorites(destination)">
                 </div>
             </div>
@@ -31,7 +31,7 @@
                 </div>
                 <h3 v-on:click='flip()' >{{ destination.name }}</h3>
                 <div class="button-container">
-                    <label for="favorite"> Add to list: </label>
+                    <label for="favorite"> Favorite: </label>
                     <input type="checkbox" id="favorite" name="favorite" value="yes" v-model="selected">
                  </div>
             </div>
