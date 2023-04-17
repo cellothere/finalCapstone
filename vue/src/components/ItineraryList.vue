52e132683d0b6967a99235db7bf48c55a63176b6
Luke Brevoort <ljbrevoort@gmail.com>
2023-04-17 16:14:45 -0400

    added framework for new itinerary view structure. Pushing and pulling to get sample data for itinerary.

---
+++ vue/src/components/ItineraryList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class='list'>
+        <itinerary-card v-for="destination in itinerary" v-bind:key="destination.id" v-bind:id="destination.id" />
+    </div>
+</template>
+
+<script>
+import itineraryCard from '../components/ItineraryCard.vue'
+import itineraryService from '../services/ItineraryService.js'
+
+export default {
+    components: {
+        itineraryCard,
+    },
+    data () {
+        return {
+        destinations: [],
+        }
+    },
+    created() {
+    itineraryService
+        .getActivitiesByUserIdAndItineraryId(this.$route.params.userId, this.$route.params.itineraryId)
+        .then((response) => {
+            this.destinations = response.data;
+    });
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/views/Itinerary2.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <itinerary-list />
+      <favorites-side-bar />
+  </div>
+</template>
+
+<script>
+import itineraryList from "../components/ItineraryList.vue";
+import favoritesSideBar from '../components/FavoritesSideBar.vue'
+
+export default {
+    components: { 
+        itineraryList,
+        favoritesSideBar
+    },
+
+}
+</script>
+
+<style>
+
+</style>
