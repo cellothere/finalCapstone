c43d42275e10436aa5c0f2e4ce3e09f2b1ea8ade
Ammar Bhatti <ammarbhatti.ab@gmail.com>
2023-04-18 16:03:25 -0400

    Updated Saved Itinerary

--- vue/src/components/SavedItineraries.vue
+++ vue/src/components/SavedItineraries.vue
@@ -2,41 +2,57 @@
   <div id="adding">
     <div id=route>
         <router-link v-bind:to="{ name: 'build itinerary'}" id="buildAddNewButton">+Add New Itinerary</router-link>
+        <button v-if="showButton" type="button" v-on:click="getAllItineraries()">Load Itineraries</button>
       </div>
-    <router-view />
-    </div>
 
+    <router-view />
+
+  <div>
+    <h2>Itineraries</h2>
+    <ul>
+      <li v-for="itinerary in itineraries" :key="itinerary.itinerary_id">
+        {{ itinerary.itineraryTitle }} - {{ itinerary.itineraryDate }} - {{ itinerary.startingTime }}
+      </li>
+    </ul>
+  </div>
+  </div>
 
     
 </template>
 
+
 <script>
-import ItineraryService from '../services/ItineraryService';
+import ItineraryService from '../services/ItineraryService.js';
 
 export default {
-    name: 'Itineraries',
-    data(){
-        return {
-            Itinerary: []
-        }
-    },
-    methods: {
-        getItineraryId(){
-            ItineraryService.getItineraryId().then((response) => {
-                this.itineraryId = response.data;   
-            });
-        }
-    },
-    created() {
-        this.getItineraryId();
+
+  name: 'saved-itinerary',
+  data() {
+    return {
+      itineraries: [],
+      showButton: true
     }
+  },
+  methods: {
+    getAllItineraries() {
+      ItineraryService.getAllItineraries()
+        .then(response => {
+        console.log(response.data)
+        this.itineraries = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
 }
 </script>
+
 
 <style>
 #route {
   margin-top: 4%;
   font-size: 18px;
-  object-fit: cover;
+  
 }
 </style>
