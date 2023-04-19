a8336db5e5c9c11109765f2628c0af6249608142
Paul Bergeron <paulbergeroncello@gmail.com>
2023-04-19 11:52:37 -0400

    adding methods and services to save itinerary page

--- vue/src/components/SavedItineraries.vue
+++ vue/src/components/SavedItineraries.vue
@@ -2,7 +2,7 @@
   <div id="adding">
     <div id=route>
         <router-link v-bind:to="{ name: 'build itinerary'}" id="buildAddNewButton">+Add New Itinerary</router-link>
-        <button v-if="showButton" type="button" v-on:click="getAllItineraries()">Load Itineraries</button>
+        <button v-if="showButton" type="button" v-on:click="getAllItineraries(), getAllItineraryActivities(22)" >Load Itineraries</button>
       </div>
 
     <router-view />
@@ -10,9 +10,13 @@
   <div>
     <h2>Itineraries</h2>
     <ul>
-      <li v-for="itinerary in itineraries" :key="itinerary.itinerary_id">
+      <li v-for="itinerary in itineraries" :key="itinerary.itinerary_id"> 
         {{ itinerary.itineraryTitle }} - {{ itinerary.itineraryDate }} - {{ itinerary.startingTime }}
+         </li>
+      <li v-for="destination in destinations" :key="destination.landmark_id" >
+        {{ destination.name }}
       </li>
+      
     </ul>
   </div>
   </div>
@@ -30,15 +34,27 @@ export default {
   data() {
     return {
       itineraries: [],
+      destinations: [],
       showButton: true
     }
   },
   methods: {
     getAllItineraries() {
-      ItineraryService.getAllItineraries()
+      let userId = this.$store.state.user.id
+      ItineraryService.getAllItinerariesByUserId(userId)
         .then(response => {
         console.log(response.data)
         this.itineraries = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }, getAllItineraryActivities(itineraryId){
+      let userId = this.$store.state.user.id
+        ItineraryService.getAllItineraryActivities(userId, itineraryId)
+                .then(response => {
+                console.log(response.data)
+                this.destinations = response.data;
         })
         .catch(error => {
           console.log(error);
