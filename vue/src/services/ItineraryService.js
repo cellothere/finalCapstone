ece5f9dd2310ceda7f64c75c5fac94a229f95e07
Paul Bergeron <paulbergeroncello@gmail.com>
2023-04-20 12:34:56 -0400

    Itinerary List fixes and additions

--- vue/src/services/ItineraryService.js
+++ vue/src/services/ItineraryService.js
@@ -21,11 +21,20 @@ export default {
       return http.get(`/itinerary/${userId}/${itineraryId}`);
     },
 
+// The following two methods should be used together. Save Itinerary returns an int
   saveItineraryInfo(userId, itinerary) {
     return http.post(`/itinerary/${userId}/create`, itinerary)
   },
+
+  updateItineraryInfo(userId, itineraryId, itinerary) {
+    return http.put(`/itinerary/${userId}/${itineraryId}/update`, itinerary)
+  },
+
   saveItineraryDestinations(userId, itineraryId, arrayOfLandmarkIds){
     return http.post(`/itinerary/${userId}/${itineraryId}/`, arrayOfLandmarkIds)
+  },
+  checkItineraryIds(itineraryId) {
+    return http.get(`/itinerary/checkId/${itineraryId}/`)
   },
   deleteItinerary(userId, itineraryId) {
     return http.delete(`/itinerary/${userId}/${itineraryId}`)
